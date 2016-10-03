786b69a507f9972aa009527f2a27c3130b755cb7
YiDang <dangyi0811@hotmail.com>
2016-10-03 19:01:58 -0400

    7:01

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -27,8 +27,9 @@ Util::Vector support(const std::vector<Util::Vector>& _shapeA, const std::vector
 //Look at the GJK_EPA.h header file for documentation and instructions
 bool isConvex(const std::vector<Util::Vector>& _shape) {
 		if (_shape.size() <= 2) {
-	std::cerr << "it`s not a shape" << std::endl;
+//	std::cout << "it`s not a shape" << std::endl;
 	}
+		int flag = 0;
 	for (int i = 0; i < _shape.size(); ++i) {
 	int next = i + 1;
 	if (next == _shape.size()) {
@@ -44,8 +45,8 @@ bool isConvex(const std::vector<Util::Vector>& _shape) {
 	Util::Vector v2 = _shape[next] - _shape[i];
 	
 	float mul = v1.z*v2.x - v1.x*v2.z;
-
-	if (mul < 0) {
+	if (i == 0) flag = flag + mul;
+	if (mul *flag< 0) {
 	//	std::cout << "not a convex" << std::endl;
 		//std::cout << _shape[before].x<<","<< _shape[before].y<<","<< _shape[before].z << std::endl;
 		//std::cout << _shape[i].x << "," << _shape[i].y << "," << _shape[i].z << std::endl;
@@ -79,6 +80,7 @@ Util::Vector getCenter(const std::vector<Util::Vector>& _shape)
 		c.y /= _shape.size();
 		c.z /= _shape.size();
 	}
+	
 	return c;
 }
 
@@ -89,32 +91,90 @@ void negate(Util::Vector &direction) {
 }
 
 
-bool containsOrigin(std::vector<Util::Vector> simplex, Util::Vector *d) {
+bool containsOrigin(std::vector<Util::Vector> &simplex, Util::Vector &d) {
+	
+	
+	Util::Vector A = simplex[simplex.size()-1]; // the last added simplex
+	Util::Vector AO = A * (-1);
+	if (simplex.size() == 3)
+	{
+
+
+		Util::Vector B = simplex[simplex.size() - 2]; // the second last added simplex
+		Util::Vector C = simplex[simplex.size() - 3];
+		
+
+		Util::Vector AB = B - A;
+		Util::Vector AC = C - A;
+		
+		Util::Vector prep_AB =  AB*(AB*AC)-AC*(AB*AB) ; // (AB X AC) X AB;  
+		Util::Vector prep_AC = AC*(AC*AB)-AB*(AC*AC) ; // (AC X AB) X AC;  
+
+		if (prep_AB*AO > 0) {
+			simplex.erase(simplex.begin()+ simplex.size() - 3);
+			d = prep_AB;
+		}
+		else {
+			if (prep_AC*AO > 0) {
+				simplex.erase(simplex.begin() + simplex.size() - 2);
+				d = prep_AC;
+			}
+			else {
+//				std::cout << "here" << std::endl;
+				return true;
+
+			}
+		}
+		
+	}
+
+	else {
+	Util::Vector B = simplex[simplex.size() - 2]; // the second last added simplex
+	Util::Vector AB = B - A;
+	
+	Util::Vector prep_AB = AO*(AB*AB) - AB*(AB*AO);
+	d = prep_AB;
+	}
+	//std::cout << "simplex's size less than 3" << std::endl;
+
+	
 	return false;
 }
 
 bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
 {
+
+	if (!isConvex(_shapeA)) {
+		std::cout << "A not convex" << std::endl;
+	}
+	if (!isConvex(_shapeB)) {
+		std::cout << "B not convex" << std::endl;
+	}
+
 	Util::Vector zero(0, 0, 0);
 	Util::Vector start_a(0, 0, 0), start_b(0, 0, 0);
 	std::vector<Util::Vector> simplex;
 	Util::Vector direction;
-	
 	direction = getCenter(_shapeB) - getCenter(_shapeA);
+//	std::cout << "d:" << direction << std::endl;
 
 	simplex.push_back(support(_shapeA, _shapeB, direction));
 
 	negate(direction);
 
+	//Util::Vector a(1, 2, 3);
+	//std::cout << (a*a) << std::endl;
 	while (true) {
 		simplex.push_back(support(_shapeA, _shapeB, direction));
-		if (simplex[simplex.size() - 1].x*direction.x+ simplex[simplex.size() - 1].y*direction.y+ simplex[simplex.size() - 1].z*direction.z<=0) {
+		if (simplex[simplex.size() - 1]*direction<=0) {
 			return false;
 		}
 		else {
-			if (containsOrigin(simplex,&direction)) {
+			if (containsOrigin(simplex,direction)) {
+			//	std::cout <<"contains origin" << std::endl;
 				return true;
 			}
+		//	std::cout << "not contains origin" << std::endl;
 		}
 	
 	}
